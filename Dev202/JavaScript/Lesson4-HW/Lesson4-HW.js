239758c852051b0fbee017fbecd0c85d46b23dc7
Kathy * <Kathy@KathyLambert.me>
2019-02-23 14:16:54 -0500

    Added stying to inputs and placeholder texts.  Added validateInfo() and initialized the form field values.

---
+++ Dev202/JavaScript/Lesson4-HW/Lesson4-HW.js
@@ -0,0 +1,45 @@
+// Requirements:
+
+// Write a series of if/else statements. Check to see if the user input validates. If the user input does NOT validate, alert a message show a error message next to each invalid input.
+
+  // First name should be at least 5 characters
+
+  // Last name should be at least 5 characters
+
+  // Age should be at least 2 characters (extra credit, validate that zip is a number)
+
+  // Address1 should be at least 10 characters
+
+  // Address2 not required
+
+
+  // City should be at least 5 characters
+
+  // State should be two characters
+
+  // zip should be at least 5 characters (extra credit, validate that zip is a number)
+
+  function validateInfo() {
+
+    // Initialize each input field.
+    let firstNameInput = document.getElementById('firstNameInputField').value
+    let lastNameInput = document.getElementById('lastNameInputField').value
+    let add1Input = document.getElementById('add1InputField').value
+    let add2Input = document.getElementById('add2InputField').value
+    let cityInput = document.getElementById('cityInputField').value
+    let stateInput = document.getElementById('stateInputField').value
+    let zipInput = document.getElementById('zipInputField').value
+
+    // console.log(firstNameInput)
+    // console.log(lastNameInput)
+    // console.log(add1Input)
+    // console.log(add2Input)
+    // console.log(cityInput)
+    // console.log(stateInput)
+    // console.log(zipInput)
+
+    // Add if statements:
+
+    // Verify if firstNameInput.length === 5; if not alert error.
+
+  }

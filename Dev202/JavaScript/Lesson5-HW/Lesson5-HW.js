1b81a9b5b42decaefc25eeeb68043efda82e74da
Kathy * <Kathy@KathyLambert.me>
2019-02-27 12:52:09 -0500

    Added and confirmed displayDay functionality for the remaining days of the week.

--- Dev202/JavaScript/Lesson5-HW/Lesson5-HW.js
+++ Dev202/JavaScript/Lesson5-HW/Lesson5-HW.js
@@ -11,13 +11,36 @@
     let d1 = new Date()
     let dateDayInt = d1.getDay()
 
+    if (dateDayInt == 0) {
+      document.getElementById('day').innerHTML = 'Sunday'
+    }
+    
+    if (dateDayInt == 1) {
+      document.getElementById('day').innerHTML = 'Monday'
+    }
+    
+    if (dateDayInt == 2) {
+      document.getElementById('day').innerHTML = 'Tuesday'
+    }
+    
     if (dateDayInt == 3) {
       document.getElementById('day').innerHTML = 'Wednesday'
 
-    } else {
-      document.getElementById('day').innerHTML = ''
     }
 
+    if (dateDayInt == 4) {
+      document.getElementById('day').innerHTML = 'Thursday'
+
+    }
+
+    if (dateDayInt == 5) {
+      document.getElementById('day').innerHTML = 'Friday'
+
+    }
+
+    if (dateDayInt == 6) {
+      document.getElementById('day').innerHTML = 'Saturday'
+
+    }
     
-    // console.log(dateDayInt)
   }

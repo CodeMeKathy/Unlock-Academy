dc7ec7b58601fe17d920ac98089454957cf3fdb4
Kathy * <Kathy@KathyLambert.me>
2019-02-27 12:40:07 -0500

    Added displayDay function for index3 to confirm functionality. Made some temp styling changes.

--- Dev202/JavaScript/Lesson5-HW/Lesson5-HW.js
+++ Dev202/JavaScript/Lesson5-HW/Lesson5-HW.js
@@ -2,3 +2,22 @@
   // Current Day of the Week
   // Current Month
   // Current Season
+
+
+  // DayOfWeek
+
+
+  function displayDay(e) {
+    let d1 = new Date()
+    let dateDayInt = d1.getDay()
+
+    if (dateDayInt == 3) {
+      document.getElementById('day').innerHTML = 'Wednesday'
+
+    } else {
+      document.getElementById('day').innerHTML = ''
+    }
+
+    
+    // console.log(dateDayInt)
+  }
